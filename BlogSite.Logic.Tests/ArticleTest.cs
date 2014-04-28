04a1dab7a0a69a98fca1eba9b8ef2beaffd6e50c
preet <preet.singh@candorsoftwaresolutions.co.uk>
2014-04-28 23:41:06 +0100

    added fluent assertions and blog post class

---
+++ BlogSite.Logic.Tests/ArticleTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+
+namespace BlogSite.Logic.Tests
+{
+    [TestFixture]
+    public class ArticleTest
+    {
+        [Test]
+        public void CanCreateaBlogPost()
+        {
+            var authorUserId = 1;
+            var title = "This is a new Article Title";
+            var content = "This is the new Article Content";
+
+            var blogArticle = new BlogPost(authorUserId, title, content);
+            blogArticle.Should().NotBeNull();
+
+        }
+    }
+
+    public class BlogPost
+    {
+        public int AuthorUserId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public BlogPost(int authorUserId, string title, string content)
+        {
+            AuthorUserId = authorUserId;
+            Title = title;
+            Content = content;
+        }
+    }
+}
